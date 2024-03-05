f3058f6a84d1d8463673f55c16a43a43c22ada1a
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-05 19:08:06 +0530

    Create file

---
+++ 3000-MaximumAreaofLongestDiagonalRectangle/3000-MaximumAreaofLongestDiagonalRectangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} dimensions
+ * @return {number}
+ */
+var areaOfMaxDiagonal = function(dimensions) {
+    let res=0
+    let dMax=0
+    for(let [a,b] of dimensions)
+    {
+        const d=(a*a)+(b*b)
+        if(d>dMax || (d===dMax && a*b>res))
+        {
+            res=a*b
+            dMax=Math.max(dMax,d)
+
+        }
+    }
+    return res
+    
+[
