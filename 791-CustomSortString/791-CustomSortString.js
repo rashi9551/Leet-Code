f42468d0a81f8b6bd8a26cc52627f7d4b2f4b6b6
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-11 14:29:47 +0530

    Create file

---
+++ 791-CustomSortString/791-CustomSortString.js
@@ -0,0 +1,26 @@
+            map.delete(order[j])
+            res+=order[j].repeat(map.get(order[j]))
+        {
+        if(map.has(order[j]))
+
+        }
+    }
+    map.forEach((value,key)=>{
+        if(value>0)
+        {
+    {
+    for(let j=0;j<order.length;j++)
+    }
+        map.set(s[i],(map.get(s[i])||0)+1)
+    {
+    for(let i=0;i<s.length;i++)
+    let res=""
+var customSortString = function(order, s) {
+    let map=new Map()
+            res+=key.repeat(value)
+        }
+    })
+    
+   
+    return res
+"
