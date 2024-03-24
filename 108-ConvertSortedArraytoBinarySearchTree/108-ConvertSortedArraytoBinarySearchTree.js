c88f35fd5860312b668cabaed4fa2887582f25fd
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-24 22:43:09 +0530

    Create file

---
+++ 108-ConvertSortedArraytoBinarySearchTree/108-ConvertSortedArraytoBinarySearchTree.js
@@ -0,0 +1,12 @@
+     if (nums.length === 0) {
+ */
+var sortedArrayToBST = function(nums) {
+ * @return {TreeNode}
+        return null;
+    }
+    
+    const mid = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[mid]);
+ * @param {number[]} nums
+    
+[
