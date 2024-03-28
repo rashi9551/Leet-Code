c1c3c7119a613a7f12438af2e75857d2d9e57c4c
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-28 14:27:08 +0530

    Create file

---
+++ 3046-SplittheArray/3046-SplittheArray.js
@@ -0,0 +1,19 @@
+        if(map.has(val))
+        {
+        }else{
+            map.set(val,1)
+        }
+    }
+            map.set(val,map.get(val)+1)
+    for(let [key,value] of map){
+        if(value>2){
+    }
+            return false
+        }
+    return true
+    
+    for(let val of nums){
+    let map=new Map()
+var isPossibleToSplit = function(nums) {
+};
+[
