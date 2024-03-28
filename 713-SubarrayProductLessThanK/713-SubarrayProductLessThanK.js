43700e2e125617d9f65b488dd9817533d8e12b8c
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-28 14:03:22 +0530

    Update file 28/03/2024, 14:03:21

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.js
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.js
@@ -1,20 +1,18 @@
-var numSubarrayProductLessThanK = function(nums, k) {
-    let count=0
-    for(i=0;i<nums.length;i++)
-    {
         let power=1
- */
- * @return {number}
- * @param {number[]} nums
- * @param {number} k
-        for(j=i;power<k;j++)
+        for(j=i;j<nums.length;j++)
         {
             power*=nums[j]
-            if(power<k)count++
-            else break
+            if(power<k)
+            {
+                count++
+            }else{
+                break
+            }
         }
     }
-    return count
-};
-/**
+    {
+    for(i=0;i<nums.length;i++)
+    let count=0
+var numSubarrayProductLessThanK = function(nums, k) {
+ */
 [
