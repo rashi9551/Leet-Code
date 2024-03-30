c27025e6a1d74bb6ed8b5e23a185532bc5cd2279
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-30 20:31:29 +0530

    Create file

---
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.js
@@ -0,0 +1,18 @@
+        while(queue.length>0 && queue[queue.length-1]<nums[i])
+        {
+    {
+    for(let i=0;i<nums.length;i++)
+    let queue=[]
+    let res=[]
+var maxSlidingWindow = function(nums, k) {
+ */
+ * @return {number[]}
+ * @param {number} k
+            queue.pop()
+        }
+        queue.push(nums[i])
+        if(i>=k-1)
+        {
+            res.push(queue[0])
+            if(queue[0]===nums[i-k+1])
+[
