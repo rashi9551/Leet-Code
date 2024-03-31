c53f6f69f06f7f8a0107dd1a87428568d1a1fd7b
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-03-31 19:49:39 +0530

    Create file

---
+++ 2444-CountSubarraysWithFixedBounds/2444-CountSubarraysWithFixedBounds.js
@@ -0,0 +1,12 @@
+    let count = 0;
+    let minPos=-1
+    let maxPos=-1
+    let end=-1
+    for (let i = 0; i < nums.length; i++) {
+        const num=nums[i]
+        if(num<minK || num>maxK)
+var countSubarrays = function(nums, minK, maxK) {
+        {
+            end=i
+        }
+[
