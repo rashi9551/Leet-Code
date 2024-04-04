d7118ea1ffc21ed08ce3a687ad81b093ea9aff37
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-04-04 12:14:34 +0530

    Create file

---
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.js
@@ -0,0 +1,14 @@
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    let i=0
+    let curr=head;
+    while(curr)
+[
