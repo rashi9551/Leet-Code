fde75c3629e30072f9bde928de7b184710c299dc
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-04-09 14:17:31 +0530

    Create file

---
+++ 2073-TimeNeededtoBuyTickets/2073-TimeNeededtoBuyTickets.js
@@ -0,0 +1,22 @@
+            arr[i]-=1
+            count++
+
+        }
+        if(i===arr.length-1)
+        {
+            i=0;
+        }else{
+            i++
+        }
+    }
+    return count
+};
+        if(arr[i]!=0){
+    {
+    let count=0
+    while(arr[k]!=0)
+    let i=0
+var timeRequiredToBuy = function(arr, k) {
+ */
+ * @return {number}
+[
