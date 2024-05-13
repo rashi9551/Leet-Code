db112ee8559010ce27541ba38b7cca15aa95ddda
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-05-13 21:13:47 +0530

    Create file

---
+++ 796-RotateString/796-RotateString.js
@@ -0,0 +1,26 @@
+ * @param {string} s
+ * @param {string} goal
+/**
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    if(s.length===goal.length)
+    {
+        const str=s+s
+        let j=0
+        let k=goal.length
+        for(let i=0;i<goal.length;i++)
+        {
+            const subs=str.substring(j,k)
+            console.log(subs)
+            if(subs===goal)return true
+            j++
+            k++
+        }
+
+    }else{
+        return false
+    }
+    return false
+};
+"
