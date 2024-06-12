69cde75351ff5ad701215518d2918d58262864b3
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-06-12 14:58:04 +0530

    Create file

---
+++ 2695-ArrayWrapper/2695-ArrayWrapper.js
@@ -0,0 +1,14 @@
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.array.reduce((acc,num)=>{
+}
+
+        return acc+=num
+    },0)
+/**
+ * @return {string}
+[
