5b10e0562df4c6fcec09d4a4a732b0c2aeb6737d
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-06-12 13:23:50 +0530

    Create file

---
+++ 3151-SpecialArrayI/3151-SpecialArrayI.js
@@ -0,0 +1,9 @@
+    for(let i=0;i<nums.length;i++){
+        if((nums[i]%2)===nums[i+1]%2){
+            return false
+var isArraySpecial = function(nums) {
+ */
+ * @return {boolean}
+ * @param {number[]} nums
+/**
+[
