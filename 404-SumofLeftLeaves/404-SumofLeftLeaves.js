0c414fecf4299b88ed2bb986cb19faa15b7d6c9c
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-06-28 21:07:28 +0530

    Update file 28/06/2024, 21:07:27

--- 404-SumofLeftLeaves/404-SumofLeftLeaves.js
+++ 404-SumofLeftLeaves/404-SumofLeftLeaves.js
@@ -1,7 +1,22 @@
- * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
     let sum =0
     function rec(root){
         if(root){
+        if(root.left&&!root.left.left&&!root.left.right){
+            console.log(root.left.val)
+            sum+=root.left.val
+        }
+        rec(root.left)
+        rec(root.right)
+        }
+    }
+    rec(root)
+    return sum
+ * @return {number}
+ * @param {TreeNode} root
+/**
+ */
+ * }
+ *     this.right = (right===undefined ? null : right)
 [
