5ea13f0c7547f5a58edabd4250b25c005c3c20c8
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-06-29 15:27:55 +0530

    Update file 29/06/2024, 15:27:55

--- 125-ValidPalindrome/125-ValidPalindrome.js
+++ 125-ValidPalindrome/125-ValidPalindrome.js
@@ -1,10 +1,8 @@
-/**
- * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s=s.toString("").replace(/[, :.]/g, '').toLowerCase()
-    console.log(s)
+    s=s.toString("").replace(/[^a-zA-Zs\d+]/g, '').toLowerCase()
     let rev=s.split('').reverse().join("")
     return s===rev
+};
 "
