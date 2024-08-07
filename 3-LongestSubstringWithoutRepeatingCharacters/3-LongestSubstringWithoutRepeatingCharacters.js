4687ec84b0da9a916917e1b1ee098429228c09b1
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2024-08-07 18:44:46 +0530

    Create file

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,34 @@
+
+    for (let end = 0; end < s.length; end++) {
+        while (seen.has(s[end])) {
+            seen.delete(s[start]);
+    let seen = new Set();
+    let start = 0;
+    let max = 0;
+var lengthOfLongestSubstring = function(s) {
+ */
+ * @return {number}
+ * @param {string} s
+/**
+"
+"abcabcbb"
+"bbbbb"
+"pwwkew"
+" "
+""
+"au"
+"kbqflpdznalsbgwumsbzyhyxvylqhseutwzgldhzfutgapcnhgminmfjmqbvkgpfe"
+3
+1
+3
+1
+0
+2
+13
+3
+1
+3
+1
+0
+2
+13
