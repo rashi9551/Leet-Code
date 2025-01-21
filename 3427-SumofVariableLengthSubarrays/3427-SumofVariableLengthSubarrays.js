22adb294966cb9386ab8539ed872cb457852604a
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-01-21 13:06:45 +0530

    Create file

---
+++ 3427-SumofVariableLengthSubarrays/3427-SumofVariableLengthSubarrays.js
@@ -0,0 +1,9 @@
+        for (let j = start; j <= i; j++) {
+            result += nums[j];
+        const start = Math.max(0, i - nums[i]);
+    for (let i = 0; i < nums.length; i++) {
+        }
+    }
+
+    return result;
+};
