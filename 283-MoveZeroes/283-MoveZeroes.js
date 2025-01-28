f74a7a083e6010a0a5895d248519906f4a1b6246
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-01-28 06:35:07 +0530

    Update file 28/01/2025, 06:35:07

--- 283-MoveZeroes/283-MoveZeroes.js
+++ 283-MoveZeroes/283-MoveZeroes.js
@@ -1,1 +1,20 @@
-/
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let isFirstZero = true
+    let j = -1
+
+    for (let i = 0; i < nums.length; i++) {
+        if (isFirstZero && nums[i] === 0) {
+            j = i
+            isFirstZero = false
+        }
+        if (j >= 0 && nums[i] != 0) {
+            let temp = nums[j]
+            nums[j] = nums[i]
+            nums[i] = temp
+            j++
+        }
+    }
+};
+ * @param {number[]} nums
