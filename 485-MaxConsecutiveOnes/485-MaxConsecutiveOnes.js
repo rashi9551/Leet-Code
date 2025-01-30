7719e40654f1220a2098726719a9286573aacad1
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-01-30 19:28:54 +0530

    Update file 30/01/2025, 19:28:54

--- 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.js
+++ 485-MaxConsecutiveOnes/485-MaxConsecutiveOnes.js
@@ -1,17 +1,7 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var findMaxConsecutiveOnes = function(nums) {
-    let count=0
-    let largest=count
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==1){
-            count++
         }else{
             count=0
         }
+        largest=Math.max(count,largest)
     }
     return largest
-            largest=Math.max(count,largest)
 };
