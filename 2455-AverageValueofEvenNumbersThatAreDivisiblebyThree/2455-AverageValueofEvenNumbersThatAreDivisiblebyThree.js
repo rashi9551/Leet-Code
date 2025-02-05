f8a338d554c5a1c179493aad60ba5cd2dfe6ac1f
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-02-05 11:33:38 +0530

    Update file 05/02/2025, 11:33:38

--- 2455-AverageValueofEvenNumbersThatAreDivisiblebyThree/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree.js
+++ 2455-AverageValueofEvenNumbersThatAreDivisiblebyThree/2455-AverageValueofEvenNumbersThatAreDivisiblebyThree.js
@@ -1,14 +1,12 @@
-    let count=0
+ * @return {number}
+ */
 var averageValue = function(nums) {
-    let sum=0
+    let count=0 ,sum=0
     for(let i=0;i<nums.length;i++){
         if(nums[i]%2===0 && nums[i]%3===0){
             sum+=nums[i]
- */
             count++
         }
     }
-    return (sum/count)>0?Math.floor(sum/count):0
+    return count===0?0:Math.floor(sum/count)
 };
- * @return {number}
- * @param {number[]} nums
