804e8d3e077539938ec8e896470ac1e068109347
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-02-07 19:30:23 +0530

    Create file

---
+++ 2427-NumberofCommonFactors/2427-NumberofCommonFactors.js
@@ -0,0 +1,10 @@
+        if (a % i === 0 && b % i === 0) {
+            count++;
+        }
+    for (let i = 1; i <= g; i++) {
+
+    let count = 0;
+    let g = gcd(a, b); // Find GCD of a and b
+    let gcd = (x, y) => y === 0 ? x : gcd(y, x % y); // Function to find GCD
+var commonFactors = function(a, b) {
+ */
