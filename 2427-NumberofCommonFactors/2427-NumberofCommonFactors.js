0345d5bab5942d6e686970307911e334cfa23227
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-02-14 21:00:11 +0530

    Update file 14/02/2025, 21:00:10

--- 2427-NumberofCommonFactors/2427-NumberofCommonFactors.js
+++ 2427-NumberofCommonFactors/2427-NumberofCommonFactors.js
@@ -1,10 +1,13 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function(a, b) {
+    let gcd = (x, y) => y === 0 ? x : gcd(y, x % y); // Function to find GCD
+    let g = gcd(a, b); // Find GCD of a and b
+    let count = 0;
+
+    for (let i = 1; i <= g; i++) {
         if (a % i === 0 && b % i === 0) {
             count++;
-        }
-    for (let i = 1; i <= g; i++) {
-
-    let count = 0;
-    let g = gcd(a, b); // Find GCD of a and b
-    let gcd = (x, y) => y === 0 ? x : gcd(y, x % y); // Function to find GCD
-var commonFactors = function(a, b) {
- */
