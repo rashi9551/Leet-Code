0d11109a884f401b0aad26ad2a420d59ec247b19
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-03-02 08:15:03 +0530

    Create file

---
+++ 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.js
@@ -0,0 +1,18 @@
+            map.set(key, (map.get(key) || 0) + value);
+            let [key, value] = nums1[i];
+        if (i < nums1.length) {
+    for (let i = 0; i < maxLen; i++) {
+
+        }
+
+        if (i < nums2.length) {
+            let [key, value] = nums2[i];
+            map.set(key, (map.get(key) || 0) + value);
+        }
+    }
+
+    return Array.from(map).sort((a, b) => a[0] - b[0]);
+    let maxLen = Math.max(nums1.length, nums2.length);
+    let map = new Map();
+var mergeArrays = function (nums1, nums2) {
+ */
