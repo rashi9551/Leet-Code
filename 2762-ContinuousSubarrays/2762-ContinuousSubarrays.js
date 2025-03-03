df30d6a41e14d08a35171cd869add663ad92cf77
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-03-03 15:54:18 +0530

    Create file

---
+++ 2762-ContinuousSubarrays/2762-ContinuousSubarrays.js
@@ -0,0 +1,17 @@
+        } else {
+            r++
+            res += (r - l + 1)
+        if (max - min >= 0 && max - min <= 2) {
+
+        max = Math.max(max, nums[r])
+        min = Math.min(min, nums[r])
+    while (r < nums.length) {
+
+    let res = 0
+    let min = nums[0], max = nums[0];
+    let l = 0, r = 0;
+var continuousSubarrays = function (nums) {
+ */
+ * @param {number[]} nums
+ * @return {number}
+/**
