225a211361de9f38941e1ca8c2a9fa37f6800520
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-03-04 09:59:32 +0530

    Update file 04/03/2025, 09:59:32

--- 1346-CheckIfNandItsDoubleExist/1346-CheckIfNandItsDoubleExist.js
+++ 1346-CheckIfNandItsDoubleExist/1346-CheckIfNandItsDoubleExist.js
@@ -1,13 +1,15 @@
+/**
  * @param {number[]} arr
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    for(let i=0 ;i<arr.length;i++){
-        for(let j=0;j<arr.length;j++){
-            if(i!=j&&arr[i]===2*arr[j]){
-                return true
-            }
-        }
+    let map = new Map()
+    for(num of arr){
+        if(map.get(2*num) || map.get(num/2)){
     }
+            return true
+        }else{
+            map.set(num,true)
+        }
     return false
 };
