513c953a71b4a337052b737ea45678eaf22940cd
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-22 16:44:28 +0530

    Update file 22/05/2025, 16:44:28

--- 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.js
+++ 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.js
@@ -3,6 +3,6 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    let arr = nums.sort((a,b)=>a-b);
-    return (arr[nums.length-2]*arr[nums.length-1])-(arr[0]*arr[1])
+    nums.sort((a,b)=>a-b);
+    return (nums[nums.length-2]*nums[nums.length-1])-(nums[0]*nums[1])
 };
