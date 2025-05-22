4fcf9017e8d610ada5b21e40798f919e9f692882
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-22 17:00:54 +0530

    Update file 22/05/2025, 17:00:53

--- 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.js
+++ 3300-MinimumElementAfterReplacementWithDigitSum/3300-MinimumElementAfterReplacementWithDigitSum.js
@@ -1,14 +1,10 @@
-        let n=nums[i]
         while(n>0){
             let ans=n%10
             n=(n-ans)/10
-        }
         let sum=0
             sum+=ans
-        nums[i]=sum
+        }
+        if(sum<small)small=sum
     }
-    for(let i = 0 ; i < nums.length ; i++){
-    return nums.sort((a,b)=>a-b)[0]
-var minElement = function(nums) {
- */
- * @return {number}
+    return small
+};
