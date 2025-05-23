8604f8adaaebc79df9ff629232c153c20635809e
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-23 16:40:55 +0530

    Update file 23/05/2025, 16:40:55

--- 2733-NeitherMinimumnorMaximum/2733-NeitherMinimumnorMaximum.js
+++ 2733-NeitherMinimumnorMaximum/2733-NeitherMinimumnorMaximum.js
@@ -1,8 +1,13 @@
-/**
- * @param {number[]} nums
- * @return {number}
+        let right = arr.slice(mid)
+        let left = arr.slice(0, mid)
+        const mid = Math.floor(arr.length / 2)
+        }
+            return arr
+        if (arr.length <= 1) {
+    function mergeSort(arr) {
+    if (nums.length <= 2) return -1
+var findNonMinOrMax = function (nums) {
  */
-var findNonMinOrMax = function(nums) {
-    if(nums.length<=2)return -1
-    return nums.sort((a,b)=>a-b)[1]
-};
+ * @return {number}
+ * @param {number[]} nums
+/**
