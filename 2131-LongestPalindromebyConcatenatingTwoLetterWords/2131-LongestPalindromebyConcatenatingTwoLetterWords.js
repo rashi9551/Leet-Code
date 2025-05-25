b7f5ed0b175776a241491a20f43a7d71956442f0
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-25 18:06:16 +0530

    Create file

---
+++ 2131-LongestPalindromebyConcatenatingTwoLetterWords/2131-LongestPalindromebyConcatenatingTwoLetterWords.js
@@ -0,0 +1,11 @@
+    for(let w of words){
+
+    let same =new Map()
+    let useMiddle=true
+    let res=0
+    let diff =new Map()
+ */
+var longestPalindrome = function(words) {
+ * @return {number}
+/**
+ * @param {string[]} words
