5f1ee933a00d0176506095de313de7d9b6ae3c77
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-30 11:32:32 +0530

    Create file

---
+++ 674-LongestContinuousIncreasingSubsequence/674-LongestContinuousIncreasingSubsequence.js
@@ -0,0 +1,15 @@
+            count++
+        }else{
+        if(nums[i]<nums[i+1]){
+        console.log(nums[i])
+    while(i<nums.length){
+
+    if(new Set(nums).size===1)return 1
+    let count = 1
+    let maxCount=-Infinity
+    let i = 0;
+var findLengthOfLCIS = function(nums) {
+ */
+ * @return {number}
+/**
+ * @param {number[]} nums
