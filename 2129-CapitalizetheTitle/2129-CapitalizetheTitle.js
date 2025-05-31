b91697e82d7ba46629c34a88c83835ea95fa1e89
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-05-31 17:11:55 +0530

    Create file

---
+++ 2129-CapitalizetheTitle/2129-CapitalizetheTitle.js
@@ -0,0 +1,9 @@
+    {
+    return arr.map(word => 
+    let arr=title.toLowerCase().split(' ')
+var capitalizeTitle = function(title) {
+ */
+ * @return {string}
+ * @param {string} title
+        if(word.length<=2){
+            return word
