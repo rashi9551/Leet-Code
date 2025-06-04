8d0dc34e5c2d3778aa1be74a5a13a6b2494b080b
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-04 13:10:35 +0530

    Update file 04/06/2025, 13:10:34

--- 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.js
+++ 448-FindAllNumbersDisappearedinanArray/448-FindAllNumbersDisappearedinanArray.js
@@ -1,26 +1,11 @@
- * @param {number[]} nums
- * @return {number[]}
- */
-var findDisappearedNumbers = function(nums) {
-    let obj={}
-    for(let value of nums){
-        obj[value]=1
-    }
+    let set=new Set(nums)
     let res=[]
     for(let i=1;i<=nums.length;i++){
-        if(!obj[i])res.push(i)
+        if(!set.has(i))res.push(i)
     }
     return res
+var findDisappearedNumbers = function(nums) {
+ */
+ * @return {number[]}
 };
-/**
-[4,3,2,7,8,2,3,1]
-[1,1]
-{ '1': 1, '2': 1, '3': 1, '4': 1, '7': 1, '8': 1 }
-
-{ '1': 1 }
-
-[5,6]
-[2]
-[5,6]
-[2]
-[
+ * @param {number[]} nums
