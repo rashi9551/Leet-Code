0faead7b6cbb43124e0ae1a6f25952dbe0210886
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-04 14:05:58 +0530

    Create file

---
+++ 748-ShortestCompletingWord/748-ShortestCompletingWord.js
@@ -0,0 +1,16 @@
+                isValid = false;
+            if ((wordCount.get(char) || 0) < count) {
+        for (let [char, count] of targetCount) {
+
+        let isValid = true;
+        const wordCount = getCharCount(word);
+    for (let word of words) {
+
+    let result = "";
+    const targetCount = getCharCount(licensePlate.replace(/[^a-z]/gi, '').toLowerCase());
+var shortestCompletingWord = function (licensePlate, words) {
+ */
+ * @return {string}
+                break;
+            }
+        }
