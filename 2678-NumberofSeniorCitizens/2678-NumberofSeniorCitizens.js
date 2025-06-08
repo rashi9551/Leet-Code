0caa8c4ab1145e0e40a313ffce75f46c387e1f33
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-08 14:13:06 +0530

    Create file

---
+++ 2678-NumberofSeniorCitizens/2678-NumberofSeniorCitizens.js
@@ -0,0 +1,11 @@
+var countSeniors = function (details) {
+    for (let str of details) {
+        if (Number(str[11] + str[12]) > 60) {
+    let res = 0
+            res++
+        }
+    }
+ */
+ * @return {number}
+/**
+ * @param {string[]} details
