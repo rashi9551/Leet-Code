5aca1381ad1ab9af36c47e9f712baac96c75c29b
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-08 14:25:23 +0530

    Create file

---
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.js
@@ -0,0 +1,11 @@
+ * @param {number} n
+ * @return {number[]}
+ */
+var lexicalOrder = function(n) {
+    const arr = [];
+    for (let i = 1; i <= n; i++) {
+        arr.push(i.toString());
+    }
+    arr.sort(); 
+    return arr.map(Number); 
+};
