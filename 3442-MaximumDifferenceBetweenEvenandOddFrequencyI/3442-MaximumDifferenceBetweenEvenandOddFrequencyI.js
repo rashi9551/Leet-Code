506ad60aa9f316f1dc33f7b6167b81cdf4063d7b
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-10 15:38:41 +0530

    Update file 10/06/2025, 15:38:41

--- 3442-MaximumDifferenceBetweenEvenandOddFrequencyI/3442-MaximumDifferenceBetweenEvenandOddFrequencyI.js
+++ 3442-MaximumDifferenceBetweenEvenandOddFrequencyI/3442-MaximumDifferenceBetweenEvenandOddFrequencyI.js
@@ -1,7 +1,17 @@
-    for(let i=0 ;i < s.length ;i++){
- * @param {string} s
- * @return {number}
- */
-var maxDifference = function(s) {
+    for (let i = 0; i < s.length; i++) {
+        map.set(s[i], (map.get(s[i]) || 0) + 1)
+    }
+    let a1 = -Infinity
+    let a2 = Infinity
+    for (let [str, freq] of map) {
+        if (freq % 2 === 0) {
+            a2 = Math.min(a2, freq)
+        } else {
+            a1 = Math.max(a1, freq)
+        }
+    }
     let map = new Map()
-/**
+var maxDifference = function (s) {
+ */
+ * @return {number}
+ * @param {string} s
