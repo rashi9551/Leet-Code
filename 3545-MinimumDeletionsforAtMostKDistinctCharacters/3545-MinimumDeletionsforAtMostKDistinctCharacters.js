fbdb7e0d3d47fea685e41892689fac62b9f1bdf8
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-11 16:12:57 +0530

    Update file 11/06/2025, 16:12:57

--- 3545-MinimumDeletionsforAtMostKDistinctCharacters/3545-MinimumDeletionsforAtMostKDistinctCharacters.js
+++ 3545-MinimumDeletionsforAtMostKDistinctCharacters/3545-MinimumDeletionsforAtMostKDistinctCharacters.js
@@ -1,13 +1,12 @@
-        console.log(l," leng ",occ," occ ", k," limit " , res ,"res")
-        if(l<=k){
+        if (l <= k) {
             return res
-    for(let [key,occ] of freq){
-    let res = 0
-    let l=freq.length
-    let freq = [...map.entries()].sort((a,b)=>a[1]-b[1])
+        }
+        l--
+        res += occ
     }
-        map.set(s[i],(map.get(s[i])||0)+1)
-    for (let i = 0; i < s.length; i++) {
-    let map = new Map()
-var minDeletion = function (s, k) {
- */
+    for (let [key, occ] of freq) {
+    let res = 0
+    let l = freq.length
+    let freq = [...map.entries()].sort((a, b) => a[1] - b[1])
+    }
+        map.set(s[i], (map.get(s[i]) || 0) + 1)
