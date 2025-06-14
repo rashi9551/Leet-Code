46f6fe92c5c8872bdbc0f1c7cda693182a3074ea
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-14 13:02:00 +0530

    Update file 14/06/2025, 13:02:00

--- 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.js
+++ 2566-MaximumDifferencebyRemappingaDigit/2566-MaximumDifferencebyRemappingaDigit.js
@@ -1,18 +1,11 @@
-    let str = num.toString()
     for (let i = 0; i < str.length; i++) {
         if (str[i] != '9') {
             first += str[i]
             break
         }
     }
-    let num1 = Number(str.replaceAll(first, 9))
+    let num1 = Number(str.replaceAll(first===''?'0':first, 9))
     let num2 = Number(str.replaceAll(str[0], 0))
+    console.log(num1,num2)
     return num1 - num2
-    let first = ''
-var minMaxDifference = function (num) {
- */
- * @return {number}
- * @param {number} num
-/**
-        if(i===str.length-1)return num
 };
