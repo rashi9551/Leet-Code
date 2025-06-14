005c8448a511587edb0b6e2ed2cbacf4b5e0b770
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-14 12:27:46 +0530

    Create file

---
+++ 771-JewelsandStones/771-JewelsandStones.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function (jewels, stones) {
+    for (let i = 0; i < stones.length; i++) {
+        if (jewels.includes(stones[i])) res++
+    let res = 0
+    }
+    return res
+};
