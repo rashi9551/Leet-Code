0214cfeb96fad85f4449181d5644657c553d4cb9
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-16 14:15:43 +0530

    Create file

---
+++ 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.js
@@ -0,0 +1,16 @@
+        }
+    }
+    return maxDiff
+};
+                let val = nums[j] - nums[i]
+                maxDiff = Math.max(maxDiff, val)
+            }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+ */
+var maximumDifference = function (nums) {
+    let maxDiff = -1
+ * @param {number[]} nums
+ * @return {number}
+/**
