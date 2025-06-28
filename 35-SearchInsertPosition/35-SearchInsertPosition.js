ead630161372d2f3d4ffc5c9d73c85bccf315954
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-28 16:43:11 +0530

    Update file 28/06/2025, 16:43:11

--- 35-SearchInsertPosition/35-SearchInsertPosition.js
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -1,7 +1,16 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var searchInsert = function(nums, target) {
-[
+        }
+        if (nums[mid] < target) {
+            return mid
+        if (nums[mid] >= target && nums[mid - 1] < target) {
+        }
+            return mid
+            return search(s + 1, e)
+        } else {
+            return search(s, e - 1)
+        }
+    }
+    return search(0, nums.length - 1)
+        if (s >= e) {
+};
+        let mid = Math.floor((s + e) / 2)
+    function search(s, e) {
