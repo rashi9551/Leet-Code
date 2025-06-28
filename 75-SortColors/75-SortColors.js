cf8e9ea39beb7e1c6517d3c52fd1aab4b9081ca4
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-28 17:00:26 +0530

    Update file 28/06/2025, 17:00:25

--- 75-SortColors/75-SortColors.js
+++ 75-SortColors/75-SortColors.js
@@ -1,18 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-var sortColors = function(arr) {
-    for(i=1;i<arr.length;i++)
-    {
-        let key=arr[i]
-        let j=i-1
-        while(j>=0&&arr[j]>key){
-            arr[j+1]=arr[j]
-            j--
-        }
-        arr[j+1]=key
+
+    const sorted = quick(nums);
+
     }
 
-    return arr
-[
+        return [...quick(left), pivot, ...quick(right)];
+        }
+            else right.push(arr[i]);
+            if (arr[i] < pivot) left.push(arr[i]);
+        for (let i = 1; i < arr.length; i++) {
+
+        let left = [], right = [];
+        let pivot = arr[0];
+
+        if (arr.length <= 1) return arr;
+    function quick(arr) {
+ */
+var sortColors = function (nums) {
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+/**
