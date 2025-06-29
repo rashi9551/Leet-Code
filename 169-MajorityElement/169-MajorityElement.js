a309df4cc6c78be2f66ce6ca2ebedc38b326a47a
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-06-29 12:08:06 +0530

    Update file 29/06/2025, 12:08:06

--- 169-MajorityElement/169-MajorityElement.js
+++ 169-MajorityElement/169-MajorityElement.js
@@ -4,9 +4,10 @@
  */
 var majorityElement = function (nums) {
     const map = new Map()
-    for (let val of nums) {
-        map.set(val, (map.get(val) || 0) + 1)
-    }
+    for (let i = 0; i < nums.length; i++) {
     res = [...map.entries()].sort((a, b) => b[1] - a[1])
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1)
     return res[0][0]
+    }
+
 };
