65aca554feef57613d151dc67794ce48b79d0e23
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-07-01 15:56:56 +0530

    Create file

---
+++ 3330-FindtheOriginalTypedStringI/3330-FindtheOriginalTypedStringI.js
@@ -0,0 +1,13 @@
+        if (word[i] === word[i + 1]) {
+    }
+            count++
+        }
+    return count + 1
+};
+    for (let i = 0; i < word.length; i++) {
+    count = 0
+var possibleStringCount = function (word) {
+ * @return {number}
+ */
+ * @param {string} word
+/**
