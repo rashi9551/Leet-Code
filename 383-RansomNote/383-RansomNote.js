f14cbae2fc3a5be1adfa4f23406ab1ea7ea6eb7f
Muhammed Rashid T <143874734+rashi9551@users.noreply.github.com>
2025-07-01 23:20:45 +0530

    Create file

---
+++ 383-RansomNote/383-RansomNote.js
@@ -0,0 +1,19 @@
+            mCount.set(letter, mCount.get(letter) - 1)
+        } else {
+            return false
+        }
+    }
+    return true
+
+};
+function getCount(str) {
+    const map = new Map()
+    for (let letter of str) {
+        if (mCount.get(letter) > 0) {
+    for (let letter of ransomNote) {
+        map.set(letter, (map.get(letter) || 0) + 1)
+    let mCount = getCount(magazine)
+var canConstruct = function (ransomNote, magazine) {
+ */
+ * @return {boolean}
+ * @param {string} magazine
